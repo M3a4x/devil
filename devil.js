8c646e1530fb16eebe10f4fd714072af22f7849b
M3a4x <38668201+M3a4x@users.noreply.github.com>
2018-08-04 10:35:19 +0300

    Update devil.js

--- devil.js
+++ devil.js
@@ -1,42 +1,22 @@
-const Eris = require("eris");
-var kboosh = new Eris("NDU1NTE1NjQ5NzYzMTgwNTU0.DkSAEw.aPlLk93jEH_SGm5LArbQ2pr5r5Y");
-var kboosh_id = "474540919396237343";
-                    var i = "0";
-                    var x = "0";
-kboosh.on("voiceChannelJoin", (msg) => {
-    x++;
-    kboosh.editChannel(kboosh_id, { name : "Devil Community ©. ⇏「" + x + "」"});
-});
-kboosh.on("voiceChannelLeave", (msg) => {
-    x--;
-    kboosh.editChannel(kboosh_id, { name : "Devil Community ©. ⇏「" + x + "」"});
-});
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-kboosh.on("messageCreate", (msg) => {
-    if(msg.author.id !== "194559857536270336") return kboosh.createMessage('__**This Command is only for the bot Owner**__');
-    if(msg.content === "#voice") {
-        let users = msg.channel.guild.members.map(m => m.user.id);
-        let messages = [];
-        messages.push(users);
-        setTimeout(function(){
-        while (i <= messages[0].length - 1) {
-            check = msg.channel.guild.members.get(messages[0][i]);
-        if(!check.voiceState.channelID){
-                i++;
-        }else{
-                x++;
-                i++;
-        }
-}
-    console.log(x);
-    kboosh.createMessage(msg.channel.id, "Voice Online Members Now Are: **"+x+"** Members!");
-    kboosh.editChannel(kboosh_id, { name : "Devil Community ©. ⇏「"+x+"」"});
-    messages = [];
-}, 1);
-    }
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '475204815736274974').setName("「");
+client.channels.find('id', '475204815736274974').setName("「 Wel");
+client.channels.find('id', '475204815736274974').setName("「 Welco");
+client.channels.find('id', '475204815736274974').setName("「 Welcome");
+client.channels.find('id', '475204815736274974').setName("「 Welcome To");
+client.channels.find('id', '475204815736274974').setName("「 Welcome To Dev");
+client.channels.find('id', '475204815736274974').setName("「 Welcome To Devil");
+client.channels.find('id', '475204815736274974').setName("「 Welcome To Devil");
+client.channels.find('id', '475204815736274974').setName("「 Welcome To Community");
+client.channels.find('id', '475204815736274974').setName("「 Welcome To Community 」");
+client.channels.find('id', '475204815736274974').setName("「 Enjoy");
+client.channels.find('id', '475204815736274974').setName("「 on");
+client.channels.find('id', '475204815736274974').setName("「 this Community 」");
+  }, 3000);
 });
 
-
-  
-
-kboosh.connect("NDU1NTE1NjQ5NzYzMTgwNTU0.DkSAEw.aPlLk93jEH_SGm5LArbQ2pr5r5Y")
+client.login('NDU1NTE1NjQ5NzYzMTgwNTU0.DkSAEw.aPlLk93jEH_SGm5LArbQ2pr5r5Y')
